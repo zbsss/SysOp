5f9899f422d42a797115faf6add01d34bcaf0409
zbsss <michal.kurleto@gmail.com>
2020-03-08 19:13:47 +0100

    add file generator

--- files/file_generator.py
+++ files/file_generator.py
@@ -3,29 +3,23 @@ import argparse
 import string
 import random
 
-#os.system("ls")
 
 #run by:
-#python3 file_generator.py -num 10 -size medium
+#python3 file_generator.py -num 10 -mw 10
 
-#lines:
-#small 1-5
-#medium 6-15
-#large  16-30
 
 def generate_file(name,args):
-    lines = random.randint(1,args.lines)
+    words = random.randint(1,args.words)
     text = ""
 
-    for i in range(lines):
+    for i in range(words):
         times = random.randint(1,10)
         letter = random.choice(string.ascii_lowercase)
-
+        
+        text += letter * times 
+        text += " "
         if random.choice([True,False]):
             text += "\n"
-        else:
-            text += letter * times 
-            text += " "
     
     with open(str(name) + ".txt",'w') as file:
         file.write(text)
@@ -38,7 +32,7 @@ def run(args):
 def main():
     parser = argparse.ArgumentParser("")
     parser.add_argument("-num",help="number of files",dest="num",type=int,required=False,default=2)
-    parser.add_argument("-maxl",help="max number of lines",dest="lines"
+    parser.add_argument("-mw",help="max number of words in file",dest="words"
                         ,type=int,required=False,default=10);
     parser.set_defaults(func=run)
     args = parser.parse_args()
